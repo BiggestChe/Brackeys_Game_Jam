08ca33e65e9b9aa5851b1e1766674a42ec223650
Bree Tassinari <147297606+BreeTass@users.noreply.github.com>
2026-08-28 16:47:57 -0700

    Changed Collision layers

--- Mechanics/brightnesslider.gd
+++ Mechanics/brightnesslider.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends CanvasLayer
 
-@onready var h_slider: HSlider = $CanvasLayer/DraggablePhysicsBody2D/HSlider
-@onready var bar_collider: CollisionShape2D = $CanvasLayer/DraggablePhysicsBody2D/barCollider
-@onready var handle_collider: CollisionShape2D = $CanvasLayer/DraggablePhysicsBody2D/handleCollider
+@onready var h_slider: HSlider = $DraggablePhysicsBody2D/HSlider
+@onready var bar_collider: CollisionShape2D = $DraggablePhysicsBody2D/barCollider
+@onready var handle_collider: CollisionShape2D = $DraggablePhysicsBody2D/handleCollider
 @onready var world_environment: WorldEnvironment = $WorldEnvironment
 
 # Called when the node enters the scene tree for the first time.
@@ -10,11 +10,6 @@ func _ready() -> void:
 	var px_size = h_slider.size * h_slider.scale
 	var t = Transform2D(0, 	Vector2(0.001, 1), 0, Vector2(-px_size.x / 2, 0))
 	bar_collider.transform = t
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _on_h_slider_value_changed(value: float) -> void:
 	world_environment.environment.adjustment_brightness = 1 - (value / 100)
